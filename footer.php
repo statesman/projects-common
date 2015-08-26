b21eea5b9ec14e549dcb640de84e426915aa8f42
Christian McDonald <cmcdonald@statesman.com>
2015-08-26 14:39:50 -0500

    updating footer to use current year via php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="row">
 <div class="large-12 medium-12 columns">
-<div class="cmFooterTerms"><p class="text-center">&#169; 2014 <a href="http://www.coxmediagroup.com" target="_blank">Cox Media Group</a>. By using this website,
+<div class="cmFooterTerms"><p class="text-center">&#169; <?php echo date("Y");?> <a href="http://www.coxmediagroup.com" target="_blank">Cox Media Group</a>. By using this website,
     you accept the terms of our <a href="http://www.mystatesman.com/visitor_agreement/">Visitor Agreement</a> and <a href=
     "http://www.mystatesman.com/privacy_policy/">Privacy Policy</a>, and understand your options regarding <a href=
     "http://www.mystatesman.com/privacy_policy/#ad-choices">Ad Choices</a><img border="0" src=
